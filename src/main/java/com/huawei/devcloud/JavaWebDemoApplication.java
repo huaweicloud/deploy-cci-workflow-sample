67d24caa0483278c714259001c61cd40543e9242
Chenyt26 <2396164919@qq.com>
2022-05-05 16:21:06 +0800

    add javaWebDemo
    Match-id-33fc45fe01b052b9d0896b5b3ffe8657896db3ca

---
+++ src/main/java/com/huawei/devcloud/JavaWebDemoApplication.java
@@ -0,0 +1,12 @@
+package com.huawei.devcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebDemoApplication.class, args);
+	}
+}
