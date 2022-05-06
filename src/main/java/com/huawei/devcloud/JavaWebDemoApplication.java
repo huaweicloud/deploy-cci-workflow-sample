72ef5e5e6047dacd8b6dda21e049dee2dc8f434d
Chenyt26 <2396164919@qq.com>
2022-05-06 17:24:28 +0800

    codecheck
    Match-id-9ac0b1ae2b75cd285faacc55df2168f9e6e15b12

--- src/main/java/com/huawei/devcloud/JavaWebDemoApplication.java
+++ src/main/java/com/huawei/devcloud/JavaWebDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaWebDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaWebDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebDemoApplication.class, args);
+    }
 }
